41814875af9fdfdd2ba093ea039db56e30753674
Alexis Marquez <alexismarq337@gmail.com>
2025-02-23 23:32:13 -0700

    Added input and output redirection

--- shell/shell.py
+++ shell/shell.py
@@ -1,7 +1,9 @@
 import os
 import sys, re, time
+outputRedirect = False
+inputRedirect = False
 if os.environ['PS1']:
-    PS1 = os.environ['PS1']
+    PS1 = os.environ.get('PS1', '$')
 else:
     PS1 = '$'
 while 1:
@@ -10,20 +12,53 @@ while 1:
     command = buff.decode()
     if command.strip() == 'exit':
         sys.exit(0)
-    if command.strip() == 'cd':
-        try:
-            os.chdir(command.split()[1])
-        except Exception as e:
-            os.write(1, str(e).encode())
+    if command.strip().startswith('cd'):
+        parts = command.split()
+        if len(parts) > 1:
+            try:
+                os.chdir(parts[1])
+            except Exception as e:
+                os.write(1, (str(e) + "\n").encode())
+        else:
+            os.write(1, "cd: missing argument\n".encode())
+
     if not command.startswith('\n'):
         rc = os.fork()
         if rc < 0:
             os.write(2, ("fork failed, returning %d\n" % rc).encode())
             sys.exit(1)
 
-        elif rc == 0:                   # child
+        elif rc == 0:# child
             string = command.strip()
             args = string.split(' ')
+            for i in range(len(args)):
+                if '>' in args:
+                    idx = args.index('>')
+                    try:
+                        filename = args[idx + 1]
+                        fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+                        os.dup2(fd, 1)
+                        os.close(fd)
+                        args = args[:idx]
+                    except IndexError:
+                        os.write(1, "Please provide a filename\n".encode())
+                        sys.exit(1)
+
+                if '<' in args:
+                    idx = args.index('<')
+                    try:
+                        filename = args[idx + 1]
+                        fd = os.open(filename, os.O_RDONLY)
+                        os.dup2(fd, 0)
+                        os.close(fd)
+                        args = args[:idx]
+                    except IndexError:
+                        os.write(1, "Please provide a filename\n".encode())
+                        sys.exit(1)
+                    except FileNotFoundError:
+                        os.write(1, "Input file not found\n".encode())
+                        sys.exit(1)
+
             for dir in re.split(":", os.environ['PATH']): # try each directory in the path
                 program = "%s/%s" % (dir, args[0])
                 # os.write(1, ("Trying to exec %s\n" % program).encode())
